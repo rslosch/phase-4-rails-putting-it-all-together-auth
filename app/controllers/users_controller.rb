97ee1dd32443a03df19d472b2b7f9e4d1020f420
Steven <slosch99@gmail.com>
2022-09-16 14:23:20 -0400

    Raise before_action :authorize into ApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-    before_action :authorize, except: [:create]
+    # before_action :authorize, except: [:create]
+    skip_before_action :authorize, only: [:create]
 
     def create
         user = User.create(user_params)
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
             session[:user_id] = user.id
             render json: user, status: :created
         else
+            # byebug
             render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
         end
     end
@@ -22,7 +24,7 @@ class UsersController < ApplicationController
         params.permit(:username, :image_url, :bio, :password, :password_confirmation)
     end
 
-    def authorize
-        return render json: {errors: "Not authorized"}, status: :unauthorized unless session.include? :user_id
-    end
+    # def authorize
+    #     return render json: {errors: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    # end
 end
