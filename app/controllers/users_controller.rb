0212f540c9e476557ba1445a2ae8e9bb683e2c0b
Steven <slosch99@gmail.com>
2022-09-16 14:09:04 -0400

    Implement Sign Up, Auto Login, Login and Logout feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    before_action :authorize, except: [:create]
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :image_url, :bio, :password, :password_confirmation)
+    end
+
+    def authorize
+        return render json: {errors: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
 end
