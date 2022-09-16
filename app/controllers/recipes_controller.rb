c8d231ad9df8d4af425bcb17c17800a5a89f00fc
Steven <slosch99@gmail.com>
2022-09-16 14:37:02 -0400

    Complete recipe list and creation feature, two tests do not pass

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
 class RecipesController < ApplicationController
+    def index
+        user = User.find_by(id: session[:user_id])
+
+        if user
+            recipes = user.recipes
+            render json: recipes, include: :user, status: :created
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        recipe = user.recipes.create(recipe_params)
+
+        if recipe.valid?
+            render json: recipe, include: :user, status: :created
+        else
+            render json: {errors: recipe.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def recipe_params
+        params.permit(:title, :instructions, :minutes_to_complete)
+    end
 end
